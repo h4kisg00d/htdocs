aa5b0c7d25ae8270548e1cfde1e5d9482e7b66d7
h4kisg00d <59872943+h4kisg00d@users.noreply.github.com>
2020-01-14 13:58:26 +0000

    Login system changes
    Improvements made

--- includes/signup.inc.php
+++ includes/signup.inc.php
@@ -53,6 +53,70 @@ else if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
 
    else {
        $sql = "SELECT uidUsers FROM users WHERE uidUsers=?";
-       $stmt = mysqli_stmt_init();
+       $stmt = mysqli_stmt_init($conn);
+
+       if (!mysqli_stmt_prepare($stmt,$sql)) {
+
+        header("Location: ../signup.php?error=SQLerror");
+
+    
+        exit(); 
+    
+       }
+       else {
+
+        mysqli_stmt_bind_param($stmt,"s",$username);
+        mysqli_stmt_execute($stmt);
+
+        mysqli_stmt_store_result($stmt);
+
+        $resultCheck = mysqli_stmt_num_rows($stmt);
+
+        if ($resultCheck >0 ) {
+            header("Location: ../signup.php?error=usernametaken&mail=".$email);
+
+    
+            exit(); 
+
+
+        }
+        else {
+
+            $sql = "INSERT INTO users (uidUsers,emailUsers,pwdUsers) VALUES (?,?,?)";
+            $stmt = mysqli_stmt_init($conn);
+            if (!mysqli_stmt_prepare($stmt,$sql)) {
+
+                header("Location: ../signup.php?error=SQLerror");
+        
+            
+                exit(); 
+            }
+
+            else {
+                $hashedPwd = password_hash($password,PASSWORD_DEFAULT);
+                mysqli_stmt_bind_param($stmt,"sss",$username,$email,$hashedPwd);
+                mysqli_stmt_execute($stmt);
+                header("Location: ../signup.php?signup=success");
+
+    
+                exit(); 
+    
+               
+
+            }
+
+
+        }
+
+       }
+
+       }
+
+       mysqli_stmt_close($stmt);
+       mysqli_close($conn);
    }
-}
+
+   else {
+header("Location: ../signup.php");
+
+   }
